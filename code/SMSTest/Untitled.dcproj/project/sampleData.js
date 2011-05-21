f3f53f0b732829b88c13b52f13cf735aa50441c2
Olivier Morandi <olivier.morandi@gmail.com>
2011-05-21 14:04:22 +0200

    updated licenses

---
+++ code/SMSTest/Untitled.dcproj/project/sampleData.js
@@ -0,0 +1,19 @@
+{
+    title: 'National Parks',
+    items: [
+        { name: "Acadia", location: "Maine, USA" }, 
+        { name: "Bryce Canyon", location: "Utah, USA" }, 
+        { name: "Carlsbad Caverns", location: "New Mexico, USA" }, 
+        { name: "Cuyahoga Valley", location: "Ohio, USA" }, 
+        { name: "Death Valley", location: "California, USA" }, 
+        { name: "Denali Preserve", location: "Alaska, USA" }, 
+        { name: "Grand Canyon", location: "Arizona, USA" }, 
+        { name: "Haleakala", location: "Hawaii, USA" }, 
+        { name: "Joshua Tree", location: "California, USA" }, 
+        { name: "Kings Canyon", location: "California, USA" }, 
+        { name: "Mesa Verde", location: "Colorado, USA" },
+        { name: "Shenandoah", location: "Virginia, USA" },
+        { name: "Yellowstone", location: "Wyoming, USA" },
+        { name: "Yosemite", location: "California, USA" }
+    ]
+}
